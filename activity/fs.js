cf6156c4bcc026b29fdcb11fa589cd1b50590bc5
vinay <vinay01431998@gmail.com>
2021-11-09 06:00:15 +0530

    Done

---
+++ activity/fs.js
@@ -0,0 +1,14 @@
+let request=require("request")
+const fs=require("fs");
+const cheerio=require("cheerio");
+let htmlKaData=fs.readFileSync("./index.html","utf8");
+//ab load karo cheerio me jispe "Document " type kaam chahye
+let myDocument=cheerio.load(htmlKaData);//html file loaded in cheerio AB KAAND KARO
+
+let h1TagData=myDocument("h1").text();
+let pTagData=myDocument("ul li p").text();
+console.log(pTagData);
+
+//console.log(h1TagData);
+//console.log(myDocument(pTagData).text());
+
